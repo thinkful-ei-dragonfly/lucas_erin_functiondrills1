ffee64e127e5911a8a62421ba8547e27d3216478
Erin Dustin <erincdustin@gmail.com>
2019-04-29 14:29:08 -0400

    created yearOfBirth variable

--- drills.js
+++ drills.js
@@ -1,7 +1,8 @@
 'use strict';
 
-function createGreeting() {
-  return 'Hi my name is Erin and I\'m 25 years old.';
+function createGreeting(name, age) {
+  let yearOfBirth = 2019 - age;
+    return `Hi my name is ${name} and I'm ${age} years old.  I was born in ${yearOfBirth}.`;
 }
 
 const greeting = createGreeting();
